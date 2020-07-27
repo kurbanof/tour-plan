d675fe1191414f120dd32c1de57d48b42e4ac182
muscleman-ak <muscleman.ak@gmail.com>
2020-07-27 21:13:46 +0700

    create: added swiper-slider

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+    loop: true,
+
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button__next',
+    prevEl: '.slider-button__prev',
+  },
+
+  effect: 'fade',
+
+})
