c0314bb22e0dce21896463a9d8b3dfe754c0e358
muscleman-ak <muscleman.ak@gmail.com>
2020-08-03 19:46:00 +0700

    create: added second slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.hotel-swiper', {
   // Optional parameters
     loop: true,
 
@@ -15,6 +15,21 @@ var mySwiper = new Swiper('.swiper-container', {
   keyboard: {
     enabled: true},
 
+});
+
+var miSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+  
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+
+  
 });
 
 
