467b02892863eb15404db19f01a2295dd1d80bbb
muscleman-ak <muscleman.ak@gmail.com>
2020-08-10 19:26:51 +0700

    fffffff

--- js/main.js
+++ js/main.js
@@ -81,6 +81,26 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay_visible");
     modalDialog.removeClass("modal__dialog_visible");
   };
+  //Обработка формы
+  $(".form").each(function() {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: "Phone number is required",
+        }
+      }
+    });
+  });
+  AOS.init();
+
 
 });
 
